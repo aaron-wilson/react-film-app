1fc61c6b76db07871e5cb52b30e158d4da80b19e
aaron-wilson <aaron-wilson@users.noreply.github.com>
2018-07-01 05:33:25 -0400

    change service worker path

--- docs/static/js/main.5539ddef.js
+++ docs/static/js/main.5539ddef.js
@@ -2750,7 +2750,7 @@ window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?
 var publicUrl=new URL("",window.location);if(publicUrl.origin!==window.location.origin){// Our service worker won't work if PUBLIC_URL is on a different origin
 // from what our page is served on. This might happen if a CDN is used to
 // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-return;}window.addEventListener('load',function(){var swUrl=""+'/service-worker.js';if(isLocalhost){// This is running on localhost. Lets check if a service worker still exists or not.
+return;}window.addEventListener('load',function(){var swUrl=""+'/react-film-app/service-worker.js';if(isLocalhost){// This is running on localhost. Lets check if a service worker still exists or not.
 checkValidServiceWorker(swUrl);// Add some additional logging to localhost, pointing developers to the
 // service worker/PWA documentation.
 navigator.serviceWorker.ready.then(function(){console.log('This web app is being served cache-first by a service '+'worker. To learn more, visit https://goo.gl/SC7cgQ');});}else{// Is not local host. Just register service worker
